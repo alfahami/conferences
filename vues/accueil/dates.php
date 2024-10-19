bc3d09d73e68bb08c037d26d6719771be95b92a5
alfahami <alfahami@olive-it.ch>
2024-10-19 17:43:11 +0100

    fix: delete php files

--- vues/accueil/dates.php
+++
@@ -1,9 +0,0 @@
-<h2>Dates importantes</h2>
-
-						<article>
-								<ul style="margin: 0; list-style: none;">
-									<li>Soumission des papiers: <b>15 Fevrier 2018</b></li>
-									<li>Notification d'acceptation: <b>1 Mars 2018</b></li>
-									<li>Jours de conférences : <b>20,21,22 Mars 2018</b></li>
-								<ul>
-						</article>
--- vues/admins/accueil.php
+++
@@ -1,5 +0,0 @@
-<h2>Bienvenue <?php echo $_SESSION['user_data']['admin_name']; ?></h2>
-<article>
-  <h3></h3>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-</article>
